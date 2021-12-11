d9befd9b580f5a704fb98c8e5cd9f6180a17333b
Conan-02 <77988458+Conan-02@users.noreply.github.com>
2021-12-11 10:51:30 +0000

    Add files via upload

---
+++ Student_Driving_license.py
@@ -0,0 +1,45 @@
+# Basic tkinter template
+from tkinter import *
+root = Tk()
+root.title("DRIVING LISENCE")
+root.geometry("400x500")
+root.configure(bg="white")
+canvas=Canvas(root,width=400,height=250)
+canvas.create_image(0,0,anchor=NW)
+canvas.create_rectangle(0, 0, 400, 45, fill="#F6091E")
+
+
+label_heading = canvas.create_text(200,20, font=('Times', '24', 'bold italic') ,text="Driving License", fill = "white")
+label_id_tag = canvas.create_text(25,60, font=('Times', '14', 'bold') ,text="ID :")
+label_name_tag = canvas.create_text(30,100, font=('Times', '12', 'bold') ,text="Name :")
+label_dob_tag = canvas.create_text(52,120, font=('Times', '12', 'bold') ,text="Date of birth :")
+label_pin_tag = canvas.create_text(32,140, font=('Times', '12', 'bold') ,text="Pin no. :")
+label_address_tag = canvas.create_text(36,160, font=('Times', '12', 'bold') ,text="Address :")
+label_vehicle_type_tag = canvas.create_text(155,180, font=('Times', '12', 'bold') ,text="Authorisation to drive the following vehicles :")
+
+label_name = Label(root)
+label_id = Label(root)
+label_address = Label(root)
+
+#add function
+def myCardDetails():
+    name = "Winnie the Pooh"
+    print(type(name))
+    driverid = 7563748
+    print(type(driverid))
+    address = "06 The Street, Somewhere"
+    print(type(address))
+
+    label_name['text'] = name
+    label_id['text'] = driverid
+    label_address['text'] = address
+#add button
+button1 = Button(root, text = "SHOW CARD", command = myCardDetails)
+button1.configure(width = 20 , activebackground = "#9EC6EE", relief= FLAT)
+button1_window = canvas.create_window(200, 235, anchor=CENTER, window=button1)
+label_id_window = canvas.create_window(80, 60, anchor=CENTER, window=label_id)
+label_name_window = canvas.create_window(100, 100, anchor=CENTER, window=label_name)
+label_address_window = canvas.create_window(130, 160, anchor=CENTER, window=label_address)
+canvas.pack()
+
+# tkinter basic template end statement
